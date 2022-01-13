f6f149985f958c9b3b7c0b8ce59200efcbfd8a15
Skarafaz <skarafaz@gmail.com>
2022-01-13 19:19:41 +0100

    dev: A346_startup.lua

--- src/A346_startup.lua
+++ src/A346_startup.lua
@@ -1,5 +1,5 @@
 -- ToLiss A346 startup script by Skarafaz
--- version 1.0.0
+-- version 1.0.1
 
 if PLANE_ICAO ~= "A346" then
     return
@@ -7,8 +7,20 @@ end
 
 local gpu1 = dataref_table("AirbusFBW/EnableExternalPower")
 local gpu2 = dataref_table("AirbusFBW/EnableExternalPowerB")
+
 local door_cargo = dataref_table("AirbusFBW/CargoDoorModeArray")
 local door_pax = dataref_table("AirbusFBW/PaxDoorModeArray")
+
+local efis_1_sel_pilot = dataref_table("sim/cockpit2/EFIS/EFIS_1_selection_pilot")
+local efis_1_sel_copilot = dataref_table("sim/cockpit2/EFIS/EFIS_1_selection_copilot")
+local efis_2_sel_pilot = dataref_table("sim/cockpit2/EFIS/EFIS_2_selection_pilot")
+local efis_2_sel_copilot = dataref_table("sim/cockpit2/EFIS/EFIS_2_selection_copilot")
+
+local pack_1_switch = dataref_table("AirbusFBW/Pack1Switch")
+local pack_2_switch = dataref_table("AirbusFBW/Pack2Switch")
+
+local nd_range_capt = dataref_table("AirbusFBW/NDrangeCapt")
+local nd_range_fo = dataref_table("AirbusFBW/NDrangeFO")
 
 start_time = os.clock() + 30
 to_init = true
@@ -33,6 +45,17 @@ function init()
             door_pax[5] = 0
             door_pax[6] = 0
             door_pax[7] = 0
+
+            efis_1_sel_pilot[0] = 1
+            efis_1_sel_copilot[0] = 1
+            efis_2_sel_pilot[0] = 1
+            efis_2_sel_copilot[0] = 1
+
+            pack_1_switch[0] = 0
+            pack_2_switch[0] = 0
+
+            nd_range_capt[0] = 0
+            nd_range_fo[0] = 0
 
             to_init = false
         end
