4a20d55df453f2cb650cbf79bcf109423994fce2
Skarafaz <skarafaz@gmail.com>
2022-04-27 23:44:27 +0200

    fix: CL60_commands.lua

--- src/CL60_commands.lua
+++ src/CL60_commands.lua
@@ -1,11 +1,9 @@
 -- HotStart CL60 custom commands script by Skarafaz
--- version 1.1.0
+-- version 1.1.1
 
 if PLANE_ICAO ~= "CL60" then
     return
 end
-
-command_once("XPRealistic/MasterSwitch")
 
 function switch_camera_sys()
     command_once("SRS/X-Camera/Toggle_X-Camera_on_or_off")
