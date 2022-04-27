cde7edbb2f83692c1fc25d2e931a5266f603071d
Skarafaz <skarafaz@gmail.com>
2022-04-27 23:29:31 +0200

    dev: CL60_commands.lua

--- src/CL60_commands.lua
+++ src/CL60_commands.lua
@@ -1,9 +1,11 @@
 -- HotStart CL60 custom commands script by Skarafaz
--- version 1.0.0
+-- version 1.1.0
 
 if PLANE_ICAO ~= "CL60" then
     return
 end
+
+command_once("XPRealistic/MasterSwitch")
 
 function switch_camera_sys()
     command_once("SRS/X-Camera/Toggle_X-Camera_on_or_off")
@@ -11,3 +13,10 @@ function switch_camera_sys()
 end
 
 create_command("skarafaz/CL60/switch_camera_sys", "Switch camera system", "switch_camera_sys()", "", "")
+
+function compact_yokes()
+    command_once("CL650/contcoll/0/compact")
+    command_once("CL650/contcoll/1/compact")
+end
+
+create_command("skarafaz/CL60/compact_yokes", "Compact yokes", "compact_yokes()", "", "")
